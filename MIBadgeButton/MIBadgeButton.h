5c3f8571fcb820aea872d64c6412d0bcd73ea991
sailingX <sailingsz8@126.com>
2021-04-12 15:49:36 +0800

    把MIBadgeButton工程改成库

---
+++ MIBadgeButton/MIBadgeButton.h
@@ -0,0 +1,18 @@
+//
+//  MIBadgeButton.h
+//  MIBadgeButton
+//
+//  Created by Hawk Lei on 2021/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MIBadgeButton.
+FOUNDATION_EXPORT double MIBadgeButtonVersionNumber;
+
+//! Project version string for MIBadgeButton.
+FOUNDATION_EXPORT const unsigned char MIBadgeButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MIBadgeButton/PublicHeader.h>
+
+
